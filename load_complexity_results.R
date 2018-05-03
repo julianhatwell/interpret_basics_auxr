464bec0b5e812b36f5fe15c8baa991d5a6362fdd
julianhatwell <julian.hatwell@googlemail.com>
2018-05-03 17:58:08 +0100

    minor changes

--- load_complexity_results.R
+++ load_complexity_results.R
@@ -1,8 +1,10 @@
 # previously completed results
 load("shape_measures_30032018.RData")
 shape_measures <- shape_measures[c("n_row", "asso", "prnc")]
-names(shp_measures) <- c("n_row_full", "asso_full", "prnc_full")
+names(shape_measures) <- c("n_row_full", "asso_full", "prnc_full")
 
 load("complexity_measures_30032018.RData")
-complexity_measures <- cbind(shp_measures, complexity_measures)
+complexity_measures <- cbind(shape_measures, complexity_measures)
 complexity_measures <- complexity_measures[c(1, 4:8, 2, 9, 3, 10:31)]
+
+save(complexity_measures, file="complexity_measures.RData")
